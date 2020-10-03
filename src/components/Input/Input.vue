b91b0d0b3930c3fa43f0b8dce502dd6add1c874f
Constantinos E. Marinos <c.marinos@afse.eu>
2020-10-03 20:34:04 +0300

    Input component attributes

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,13 +1,60 @@
 <template>
-    <div>
-        <slot></slot>
+    <div class="control">
+        <input
+                class="input"
+                :class="customClass"
+                :type="type"
+                :placeholder="placeholder"
+                v-model="value"
+        >
     </div>
 </template>
 
 <script>
     export default {
-        name: 'vx-input'
+        name: 'vx-input',
+        props: {
+            type: {
+                type: String,
+                default: 'text'
+            },
+            modelValue: [Number, String],
+            customClass: {
+                type: String,
+                default: ''
+            },
+            placeholder: {
+                type: String,
+                default: ''
+            },
+            maxlength: {
+                type: Number
+            },
+            minlength: {
+                type: Number
+            },
+        },
+        data() {
+            return {
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+            value: {
+                get() {
+                    return this.modelValue
+                },
+                set(value) {
+                    this.$emit('input', value)
+                }
+            }
+        }
     }
 </script>
 
-<style></style>
+<style>
+
+
+</style>
