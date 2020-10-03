eb630ffda4797316a7e3755b8e96644c79b3e95b
Constantinos E. Marinos <c.marinos@afse.eu>
2020-10-03 10:22:49 +0300

    VueVix component library

---
+++ docs/.vuepress/components/examples/button.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <Button>This is slot content 1.</Button>
+
+        <Button>This is slot content 2.</Button>
+    </div>
+</template>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+// config.js
+
+module.exports = {
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'VueVix',
+            description: 'Documentation site for the VueVix component library plugin'
+        }
+    },
+
+    themeConfig: {
+        repoLabel: 'Contribute!',
+        repo: '',
+        docsDir: 'docs',
+        editLinks: true,
+        docsBranch: 'dev',
+        editLinkText: 'Help us improve this page!',
+        search: false,
+        locales: {
+            '/': {
+                label: 'English',
+                selectText: 'Languages',
+                lastUpdated: 'Last Updated',
+                // service worker is configured but will only register in production
+                serviceWorker: {
+                    updatePopup: {
+                        message: 'New content is available.',
+                        buttonText: 'Refresh'
+                    }
+                },
+                nav: [
+                    { text: 'Getting Started', link: '/guide' },
+                    { text: 'Components', link: '/components/' },
+                    { text: 'GitHub', link: '' }
+                ],
+                sidebar: {
+                    '/components/': [
+                        {
+                            title: 'Components',
+                            collapsable: false,
+                            children: [ 'button']
+                        }
+                    ]
+                }
+            }
+        }
+    }
+}
